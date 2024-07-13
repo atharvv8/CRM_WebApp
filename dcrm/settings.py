b15eab548b10714a6b1025384949cc6ae08c890e
Atharv <atharvg014@gmail.com>
2024-07-13 12:12:23 +0530

    fixed env

--- dcrm/settings.py
+++ dcrm/settings.py
@@ -15,6 +15,7 @@ import dj_database_url
 from dotenv import load_dotenv
 import os
 
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -30,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [] = os.environ.get('ALLOWED_HOSTS')
 # RENDER_EXTERNAL_HOSTNAME = os.environ.get('ALLOWED_HOSTS').split(" ")
 
 
